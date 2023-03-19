ad2c188acb51e54024c3d43d21a55778a2de4ca2
khadija-chennaoui <khadijach896@gmail.com>
2023-03-20 00:31:20 +0100

    création Endpoint de l'app

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
-const app = express()
-require("dotenv").config();
+const errorHandler = require('./middlewares/ErrorHandler')
+const roles = require('./routes/roleRoute')
+const User = require('./routes/userRoute')
 require('./configs/db')
+const app = express()
 app.use(express.json())
-app.use(express.urlencoded({ extended:true}))
-const PORT = process.env.PORT 
+
+app.use(express.urlencoded({ extended: true }))
+app.use('/addRole', roles)
+app.use('/Auth', User)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`server is runing at PORT ${PORT}`)
