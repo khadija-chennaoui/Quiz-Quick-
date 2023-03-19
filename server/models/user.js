0f7b416ff7f6df3014dd6d127e2d860c81a8c977
khadija-chennaoui <khadijach896@gmail.com>
2023-03-20 00:31:56 +0100

    modification model user

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,6 @@ const UserSchema = mongoose.Schema({
     name: { type: String, require: true },
     email: { type: String, require: true },
     password: { type: String, require: true },
-    roles: [{ type: mongoose.Schema.Types.ObjectId, ref: "role" }]
+    role_id: [{ type: mongoose.Schema.Types.ObjectId, ref: "role" }]
 })
 module.exports = mongoose.model('User', UserSchema)
