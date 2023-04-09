149aaea37496d5c8d8325ba4797d681856608230
khadija-chennaoui <khadijach896@gmail.com>
2023-04-09 22:05:00 +0000

    suppretion de store

--- frontend/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authSlice from "./redux/authSlice"
-const store = configureStore({
-    reducer: {
-        role: authSlice
-    }
-})
-export default store
