961f8eea00fd2de74fbf147db9987a62138b20ca
khadija-chennaoui <khadijach896@gmail.com>
2023-04-09 12:39:48 +0000

    l'ajout de quiz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import AddEtud from "./components/Dashboard/AddEtud";
+import { AddQuiz } from "./components/Dashboard/AddQuiz";
 import Dashboard from "./components/Dashboard/dashboard";
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
 
                 <Route path="/dash" element={<Dashboard />} >
                     <Route path="/dash/AddEtud" element={<AddEtud />} />
+                    <Route path="/dash/AddQuiz" element={<AddQuiz />} />
 
                 </Route>
             </Routes>
